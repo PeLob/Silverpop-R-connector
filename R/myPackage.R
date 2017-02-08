43ee7a27dec6ef535b60233f31b88237548d02bd
Pablo Maldonado <pablo.maldonado@actum.cz>
2017-02-08 11:22:09 +0100

    first

--- R/myPackage.R
+++ R/myPackage.R
@@ -3,15 +3,82 @@
 #' @import keboola.r.docker.application
 #' @export
 #' @param datadir Path to data directory.
-doSomething <- function(datadir) {
-  # read input
-  data <- read.csv(file = file.path(datadir, "in/tables/source.csv"));
+requestSilverpopEvents <- function(datadir) {
+   
+  
+	# do something
+	app <- DockerApplication$new(datadir)
+	app$readConfig()
+	
+	
+	apiURL <- app$getParameters()$apiURL
+	Email <- app$getParameters()$Email
+	UserName <- app$getParameters()$UserName
+	PassWord <- app$getParameters()$PassWord
+	StartDate <- app$getParameters()$StartDate
+	EndDate <- app$getParameters()$EndDate
+		
+	  
+	# Authentication request
+	body1 <- "<Envelope><Body>
+	<Login>
+	<USERNAME>UserName</USERNAME>
+	<PASSWORD>PassWord</PASSWORD>
+	</Login>
+	</Body></Envelope>"
 
-  # do something
-  app <- DockerApplication$new(datadir)
-  app$readConfig()
-  data['double_number'] <- data['number'] * app$getParameters()$multiplier
+	body1 <- gsub("UserName", UserName, body1)
+	body1 <- gsub("PassWord", PassWord, body1)
+
+	test1 <- POST(url = apiURL, body = body1, 
+				  verbose(), content_type("text/xml"))
+
+
+	parsed <- htmlParse(test1)
+	js <- xpathSApply(parsed, "//session_encoding", xmlValue)
+
+	#Gather sessionid for further requests
+	jsessionid <- gsub(";","?",js)
+
+
+	## Date parameters: COULD WE PASS DATE PARAMETERS FROM THE ORCHESTRATION LAYER?
+	body2 <- "<Envelope>
+			  <Body>   
+				<RawRecipientDataExport>    
+				<EVENT_DATE_START>StartDate</EVENT_DATE_START>    
+				<EVENT_DATE_END>EndDate</EVENT_DATE_END>    
+				<MOVE_TO_FTP>TRUE</MOVE_TO_FTP> 
+				<EXPORT_FORMAT>0</EXPORT_FORMAT>   
+				<EMAIL>Email</EMAIL>    
+				<ALL_EVENT_TYPES/>    
+				<INCLUDE_INBOX_MONITORING/>   
+				</RawRecipientDataExport>  
+			  </Body> 
+			  </Envelope>"
+
+	# Trick to pass the parameters
+	body2 <- gsub("StartDate", StartDate, body2)
+	body2 <- gsub("EndDate", EndDate, body2)
+	body2 <- gsub("Email", Email, body2)
+
+	test2 <- POST(url = paste(apiURL,jsessionid,sep=""), body = body2
+				  ,verbose(), content_type("text/xml"))
+
+
+	xml_data <- xmlParse(test2)
+
+	## 
+	nodes <- getNodeSet(xml_data, "//FILE_PATH")
+
+	## Parse response to Data Frame
+	data <- xmlToDataFrame(nodes)
+
+	fname <- as.character(data[[1]])
+	
+	fname_df <- data.frame(filename = fname)
+ 
+
 
   # write output
-  write.csv(data, file = file.path(datadir, "out/tables/result.csv"), row.names = FALSE)
+  write.csv(fname_df, file = file.path(datadir, "out/tables/result.csv"), row.names = FALSE)
 }
